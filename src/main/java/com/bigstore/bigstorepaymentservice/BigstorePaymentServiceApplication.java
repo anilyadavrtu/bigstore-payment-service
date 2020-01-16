1ab7308816f89ea1423d72c159cfc03bf964c3be
anilyadavrtu <anilyadav.rtu@gmail.com>
2020-01-16 17:55:31 +0530

    base project setup

---
+++ src/main/java/com/bigstore/bigstorepaymentservice/BigstorePaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bigstore.bigstorepaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigstorePaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigstorePaymentServiceApplication.class, args);
+	}
+
+}
